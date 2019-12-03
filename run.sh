a4e4e01523b4ab2dd5278fe0b317ccc30b6b879b
Mehdi Raza <mehdi.raza@venturedive.com>
2019-12-03 20:51:56 +0500

    Docker Final

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 docker-compose up -d --build database && docker-compose up -d --build app && docker exec laravel_app php artisan key:generate &&
-    docker exec laravel_app php artisan config:cache && docker exec laravel_app php artisan migrate:fresh && docker-compose up -d --build web && docker-compose up -d --build client
+    docker exec laravel_app php artisan config:cache && docker exec laravel_app php artisan migrate:fresh && docker exec laravel_app php artisan l5-swagger:generate &&
+    docker-compose up -d --build web && docker-compose up -d --build client
